87efdfd407073ec38c486a69b5e2c020d816db2f
Unknown <parsa2000kurd@gmail.com>
2018-11-29 12:49:59 -0500

    Key bindings and cleanup

--- src/me/zombies/GUI.java
+++ src/me/zombies/GUI.java
@@ -7,28 +7,51 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
 
+import javax.swing.AbstractAction;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
 
 
 public class GUI extends JFrame {
+	int playerX;
+	int playerY;
+	static final int playerV = 10;
+	//for key binding
+	private static final int IFW = JComponent.WHEN_IN_FOCUSED_WINDOW;
+	private static final String MOVE_UP = "move up";
+    private static final String MOVE_DOWN = "move down";
+    private static final String MOVE_LEFT = "move left";
+    private static final String MOVE_RIGHT = "move right";
+	
+
 	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
+		
 				new GUI();
 			}
-		});
-	} 
-	
 	GUI(){
-		this.add(new DrawPanel());	
+		DrawPanel pan = new DrawPanel();
+		
 		this.setTitle("Main graphics ..."); 
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+		this.add(pan);	
 		this.pack();
 		this.setVisible(true);
+		
+		//key bindings
+		pan.getInputMap(IFW).put(KeyStroke.getKeyStroke("UP"), MOVE_UP);
+		pan.getInputMap(IFW).put(KeyStroke.getKeyStroke("DOWN"), MOVE_DOWN);
+		pan.getInputMap(IFW).put(KeyStroke.getKeyStroke("LEFT"), MOVE_LEFT);
+		pan.getInputMap(IFW).put(KeyStroke.getKeyStroke("RIGHT"), MOVE_RIGHT);
+		//binding keys to actions
+		pan.getActionMap().put(MOVE_UP, new MoveAction(1));
+		pan.getActionMap().put(MOVE_DOWN, new MoveAction(2));
+		pan.getActionMap().put(MOVE_LEFT, new MoveAction(3));
+		pan.getActionMap().put(MOVE_RIGHT, new MoveAction(4));
 	}
 	
 	class DrawPanel extends JPanel {
@@ -55,7 +78,25 @@ public class GUI extends JFrame {
 			g.setColor(Color.BLUE);
 			g.drawOval(panSize/2-r/2, panSize/2-r/2, r, r);			
 		}
+			
 		
-		
-	}	
+	}
+	
+	private class MoveAction extends AbstractAction {
+		int direction;
+        MoveAction(int direction) {
+
+            this.direction = direction;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+        	//decrease and decrease playerX and playerY
+        	//if 1 go up by v
+        	//if 2 go down by v
+        	//if 3 go left by v
+        	//if 4 go right by v
+          
+        }
+    }
 }

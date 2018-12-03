d28961a39d2a107b4a1645c991aa7eb2ffee16b7
Callypigian <morlbren201@gotvdsb.ca>
2018-12-03 13:05:19 -0500

    added keyhandling

--- src/me/zombies/GUI.java
+++ src/me/zombies/GUI.java
@@ -9,6 +9,8 @@ import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
 import javax.swing.AbstractAction;
@@ -24,7 +26,6 @@ import javax.swing.Timer;
 public class GUI extends JFrame {
 	DrawPanel pan;
 	ArrayList<Enemy> birds;
-	ArrayList<Bullet> bullets;
 	int playerX = 100;
 	int playerY = 100;
 	static final int playerV = 10;
@@ -34,8 +35,9 @@ public class GUI extends JFrame {
     private static final String MOVE_DOWN = "move down";
     private static final String MOVE_LEFT = "move left";
     private static final String MOVE_RIGHT = "move right";
-    private static final String SHOOT_BULLET = "shoot";
-    static final int T1_SPEED = 100;
+    private final int numKeyCodes = 256;
+    private boolean[] keysDown = new boolean [numKeyCodes];
+    static final int T1_SPEED = 33;
 	
 
 	public static void main(String[] args) {
@@ -44,13 +46,12 @@ public class GUI extends JFrame {
 			}
 	GUI(){
 		birds = new ArrayList<Enemy>();
-		bullets = new ArrayList<Bullet>();
 		//create 5 enemies
 		for(int i = 0; i < 5; i++) {
 			birds.add(new Enemy(i*50, i*40));
 		}
 		pan = new DrawPanel();
-		
+		pan.addKeyListener(new KL());
 		Timer firstTimer = new Timer(T1_SPEED,new Timer1Listener());
 		firstTimer.start();
 		this.setTitle("Main graphics ..."); 
@@ -59,19 +60,17 @@ public class GUI extends JFrame {
 		this.pack();
 		this.setVisible(true);
 		
-		//key bindings
-		pan.getInputMap(IFW).put(KeyStroke.getKeyStroke("UP"), MOVE_UP);
-		pan.getInputMap(IFW).put(KeyStroke.getKeyStroke("DOWN"), MOVE_DOWN);
-		pan.getInputMap(IFW).put(KeyStroke.getKeyStroke("LEFT"), MOVE_LEFT);
-		pan.getInputMap(IFW).put(KeyStroke.getKeyStroke("RIGHT"), MOVE_RIGHT);
-		pan.getInputMap(IFW).put(KeyStroke.getKeyStroke('j'), SHOOT_BULLET);
-		//binding keys to actions
-		pan.getActionMap().put(MOVE_UP, new MoveAction(1));
-		pan.getActionMap().put(MOVE_DOWN, new MoveAction(2));
-		pan.getActionMap().put(MOVE_LEFT, new MoveAction(3));
-		pan.getActionMap().put(MOVE_RIGHT, new MoveAction(4));
-		pan.getActionMap().put(SHOOT_BULLET, new MoveAction(10));
-		
+//		//key bindings
+//		pan.getInputMap(IFW).put(KeyStroke.getKeyStroke("UP"), MOVE_UP);
+//		pan.getInputMap(IFW).put(KeyStroke.getKeyStroke("DOWN"), MOVE_DOWN);
+//		pan.getInputMap(IFW).put(KeyStroke.getKeyStroke("LEFT"), MOVE_LEFT);
+//		pan.getInputMap(IFW).put(KeyStroke.getKeyStroke("RIGHT"), MOVE_RIGHT);
+//		//binding keys to actions
+//		pan.getActionMap().put(MOVE_UP, new MoveAction(1));
+//		pan.getActionMap().put(MOVE_DOWN, new MoveAction(2));
+//		pan.getActionMap().put(MOVE_LEFT, new MoveAction(3));
+//		pan.getActionMap().put(MOVE_RIGHT, new MoveAction(4));
+//		
 
 	}
 	
@@ -81,11 +80,11 @@ public class GUI extends JFrame {
 			for (Enemy i : birds) {
 				i.moveToPosition(playerX, playerY);
 			}
+			pan.repaint();
 		}
 	}
 	
 	class DrawPanel extends JPanel {
-					
 		int panSize=600;
 		
 		DrawPanel() {	
@@ -97,6 +96,7 @@ public class GUI extends JFrame {
 		public void paintComponent(Graphics g) {
 			super.paintComponent(g); //clear screen and repaint using background colour
 			panSize = this.getWidth();
+			this.requestFocus();
 			
 			Graphics2D g2 = (Graphics2D) g;		
 			g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
@@ -104,42 +104,47 @@ public class GUI extends JFrame {
 				g2.drawRect((int)i.getX(),(int)i.getY(), 2,2);
 				
 			}
-			for( Bullet i : bullets) {
-				g2.drawRect((int)i.getX(),(int)i.getY(), 2,2);
-				i.updatePosition();
-			}
 			g2.drawRect(playerX, playerY, 10, 10);
-			pan.repaint();
+			
 		}
 			
 		
 	}
 	
-	private class MoveAction extends AbstractAction {
-		int direction;
-        MoveAction(int direction) {
+//	private class MoveAction extends AbstractAction {
+//		int direction;
+//        MoveAction(int direction) {
+//
+//            this.direction = direction;
+//        }
+//
+//        @Override
+//        public void actionPerformed(ActionEvent e) {
+//    		if (.getKeyCode()==KeyEvent.VK_KP_UP) playerY--;		
+//    		if (isKeyDown(KeyEvent.VK_C)) playerX++;
+//    		if (isKeyDown(KeyEvent.VK_S)) playerX--;
+//    		if (isKeyDown(KeyEvent.VK_X)) playerY++;
+//    		pan.repaint(); 
+//        }
+//    }
+	
+	
+	//BRENNAN CHANGES//
+	class KL implements KeyListener{
+		@Override
+		public void keyPressed(KeyEvent keyP) {
+			if (keyP.getKeyCode()==KeyEvent.VK_UP) {
+				playerY--;
+				System.out.println("nuts");
+			}
+			
+		}
+		@Override
+		public void keyReleased(KeyEvent keyP) {
+		}
+		@Override
+		public void keyTyped(KeyEvent arg0) {
+		}
+	}
 
-            this.direction = direction;
-        }
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-        	if (direction ==1) {
-        		playerY -= playerV;
-        	}
-        	else if (direction ==2) {
-        		playerY += playerV;
-        	}
-        	else if (direction == 3) {
-        		playerX -= playerV;
-        	}
-        	else if (direction == 4) {
-        		playerX += playerV;
-        	}
-        	else if (direction == 10) {
-        		bullets.add(new Bullet(playerX,playerY));
-        	}
-          pan.repaint();
-        }
-    }
 }
